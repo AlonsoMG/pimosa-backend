53aa658c0eb470ac999a77d8f7860c3ee1bcf5d7
Alonso Moreno <42045349+AlonsoMG@users.noreply.github.com>
2020-01-19 13:50:52 -0600

    Create machinery select inflators, create machienry method
    Created methods to fetch the information to inflate the machinery creation forms. Also, created the method to insert a new machinery to the database.

--- src/api/main.js
+++ src/api/main.js
@@ -4,8 +4,28 @@ const APP = SETTINGS.APP;
 const POOL = SETTINGS.POOL;
 const PORT = SETTINGS.PORT;
 
+APP.post("/machinery/machinery/create", (request, response) => {
+    MACHINERY_ENDPOINTS.createMachinery(request, response, POOL);
+});
+
+APP.post("/machinery/displacement/list", (request, response) => {
+    MACHINERY_ENDPOINTS.getDisplacementList(request, response, POOL);
+});
+
 APP.post("/machinery/machinery-type/list", (request, response) => {
     MACHINERY_ENDPOINTS.getMachineryTypeList(request, response, POOL);
+});
+
+APP.post("/machinery/operation/list", (request, response) => {
+    MACHINERY_ENDPOINTS.getOperationList(request, response, POOL);
+});
+
+APP.post("/machinery/power-source/list", (request, response) => {
+    MACHINERY_ENDPOINTS.getPowerSourceList(request, response, POOL);
+});
+
+APP.post("/machinery/work/list", (request, response) => {
+    MACHINERY_ENDPOINTS.getWorkList(request, response, POOL);
 });
 
 APP.listen(PORT, () => {
--- src/database/machinery/queries.js
+++ src/database/machinery/queries.js
@@ -1,5 +1,81 @@
 const mssql = require("mssql");
 
+const createMachinery = (request, response, pool) => {
+    const { 
+        code,
+        name,
+        brand,
+        price,
+        maintenanceCode,
+        acquisitionDate,
+        type,
+        powerSource,
+        displacement,
+        operation,
+        work,
+    } = request.body;
+    pool.then(pool => {
+        const request = new mssql.Request(pool);
+        request.input("inCode", mssql.VarChar(254), code);
+        request.input("inName", mssql.VarChar(254), name);
+        request.input("inBrand", mssql.VarChar(254), brand);
+        request.input("inPrice", mssql.Money, price);
+        request.input("inMaintenanceCost", mssql.Money, maintenanceCode);
+        request.input("inAcquisitionDate", mssql.Date, acquisitionDate);
+        request.input("inType", mssql.UniqueIdentifier, type);
+        request.input("inPowerSource", mssql.UniqueIdentifier, powerSource);
+        request.input("inDisplacement", mssql.UniqueIdentifier, displacement);
+        request.input("inOperation", mssql.UniqueIdentifier, operation);
+        request.input("inWork", mssql.UniqueIdentifier, work);
+        request.output("outCreated", mssql.Bit);
+        request.output("outMessage", mssql.VarChar(254));
+        request.output("outMachineryPK", mssql.UniqueIdentifier);
+        request.execute("uspCreateMachinery", (error, results) => {
+            if (error) {
+                response
+                    .status(500)
+                    .json({
+                        "error": true,
+                        "message": error.message
+                    });
+            }
+            response.status(200).json(results.output);
+        })
+    }).catch(error => {
+        response
+            .status(500)
+            .json({
+                "error": true,
+                "message": error.message
+            });
+    });
+}
+
+const getDisplacementList = (request, response, pool) => {
+    const { } = request.body;
+    pool.then(pool => {
+        const request = new mssql.Request(pool);
+        request.execute("uspGetDisplacementList", (error, results) => {
+            if (error) {
+                response
+                    .status(500)
+                    .json({
+                        "error": true,
+                        "message": error.message
+                    });
+            }
+            response.status(200).json(results.recordset);
+        })
+    }).catch(error => {
+        response
+            .status(500)
+            .json({
+                "error": true,
+                "message": error.message
+            });
+    });
+}
+
 const getMachineryTypeList = (request, response, pool) => {
     const { } = request.body;
     pool.then(pool => {
@@ -13,7 +89,82 @@ const getMachineryTypeList = (request, response, pool) => {
                         "message": error.message
                     });
             }
-            response.status(200).json(results.recordsets);
+            response.status(200).json(results.recordset);
+        })
+    }).catch(error => {
+        response
+            .status(500)
+            .json({
+                "error": true,
+                "message": error.message
+            });
+    });
+}
+
+const getOperationList = (request, response, pool) => {
+    const { } = request.body;
+    pool.then(pool => {
+        const request = new mssql.Request(pool);
+        request.execute("uspGetOperationList", (error, results) => {
+            if (error) {
+                response
+                    .status(500)
+                    .json({
+                        "error": true,
+                        "message": error.message
+                    });
+            }
+            response.status(200).json(results.recordset);
+        })
+    }).catch(error => {
+        response
+            .status(500)
+            .json({
+                "error": true,
+                "message": error.message
+            });
+    });
+}
+
+const getPowerSourceList = (request, response, pool) => {
+    const { } = request.body;
+    pool.then(pool => {
+        const request = new mssql.Request(pool);
+        request.execute("uspGetPowerSourceList", (error, results) => {
+            if (error) {
+                response
+                    .status(500)
+                    .json({
+                        "error": true,
+                        "message": error.message
+                    });
+            }
+            response.status(200).json(results.recordset);
+        })
+    }).catch(error => {
+        response
+            .status(500)
+            .json({
+                "error": true,
+                "message": error.message
+            });
+    });
+}
+
+const getWorkList = (request, response, pool) => {
+    const { } = request.body;
+    pool.then(pool => {
+        const request = new mssql.Request(pool);
+        request.execute("uspGetWorkList", (error, results) => {
+            if (error) {
+                response
+                    .status(500)
+                    .json({
+                        "error": true,
+                        "message": error.message
+                    });
+            }
+            response.status(200).json(results.recordset);
         })
     }).catch(error => {
         response
@@ -26,5 +177,10 @@ const getMachineryTypeList = (request, response, pool) => {
 }
 
 module.exports = {
-    getMachineryTypeList
+    createMachinery,
+    getDisplacementList,
+    getMachineryTypeList,
+    getOperationList,
+    getPowerSourceList,
+    getWorkList
 }
